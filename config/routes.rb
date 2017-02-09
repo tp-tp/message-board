f482f08d9f3620a2379a50dce6b92fcb462706e8
toshihirooda <o.d.a.t.o.p.p.y@gmail.com>
2017-02-09 00:55:31 +0000

    create partial template

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'messages#index'
   #resources :messages
   resources 'messages' , only: [:create]
+  resources 'messages' , except: [:index, :new]
   #get 'messages/index' , to: 'messages#index'
   #get 'test' , to: 'messages#test'
 
