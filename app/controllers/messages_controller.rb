b449396c22c41cfff2d38ef3a3cc35e10377f6ee
toshihirooda <o.d.a.t.o.p.p.y@gmail.com>
2017-02-13 04:34:10 +0000

    revise

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,6 @@ class MessagesController < ApplicationController
   end
   
   def create
-    binding.pry
     @message = Message.new(message_params)
     if @message.save
       redirect_to root_path , notice: 'メッセージを保存しました' 
